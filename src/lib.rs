8765acf6ef7a93db322baabb40df1edfc405b437
Martinez <Heliozoa@users.noreply.github.com>
2020-08-12 08:38:07 +0300

    Implement std::error::Error for trash::Error (#17)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::error;
+use std::fmt;
 use std::path::Path;
 
 #[cfg(test)]
@@ -32,6 +34,30 @@ pub enum Error {
 		code: Option<i32>,
 	},
 }
+
+impl fmt::Display for Error {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		match self {
+			Self::Unknown => write!(f, "Unknown error"),
+			Self::CanonicalizePath { code } => {
+				let code_str = match code {
+					Some(i) => format!("Error code was {}", i),
+					None => "No error code was available".into(),
+				};
+				write!(f, "Error while canonicalizing path. {}", code_str)
+			}
+			Self::Remove { code } => {
+				let code_str = match code {
+					Some(i) => format!("Error code was {}", i),
+					None => "No error code was available".into(),
+				};
+				write!(f, "Error while performing the remove operation. {}", code_str)
+			}
+		}
+	}
+}
+
+impl error::Error for Error {}
 
 /// Removes a single file or directory.
 ///
