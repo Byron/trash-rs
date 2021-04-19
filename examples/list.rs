a18d055e684589eb2f9176ae6536ec433b023dc1
Artúr Kovács <kovacs.artur.barnabas@gmail.com>
2021-04-19 21:52:38 +0200

    Update the list example

--- examples/list.rs
+++ examples/list.rs
@@ -5,7 +5,19 @@ fn main() {
 
 #[cfg(any(target_os = "windows", all(unix, not(target_os = "macos"))))]
 fn main() {
-    use trash;
+    use chrono::{DateTime, Local, NaiveDateTime, Utc};
     let trash_items = trash::os_limited::list().unwrap();
-    println!("{:#?}", trash_items);
+
+    let now = Local::now();
+    let long_time_ago = now - chrono::Duration::days(42);
+    let old_count = trash_items
+        .iter()
+        .filter(|item| {
+            let naive_deletion_utc = NaiveDateTime::from_timestamp(item.time_deleted, 0);
+            let deletion = DateTime::<Utc>::from_utc(naive_deletion_utc, Utc);
+            deletion < long_time_ago
+        })
+        .count();
+
+    println!("There are {} old items in your trash.", old_count);
 }
