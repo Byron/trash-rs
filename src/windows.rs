3b8370c3500f245af27c1d140a7d27ccfcbe4e46
Reilly Wood <reilly.wood@icloud.com>
2022-05-22 12:56:39 -0700

    Tests passing! Still need to make sure I'm not leaking memory

--- src/windows.rs
+++ src/windows.rs
@@ -1,23 +1,16 @@
 use std::{
     ffi::{c_void, OsStr, OsString},
-    mem::{size_of, MaybeUninit},
-    ops::DerefMut,
-    os::{
-        raw::c_int,
-        windows::{ffi::OsStrExt, prelude::*},
-    },
+    mem::MaybeUninit,
+    os::windows::{ffi::OsStrExt, prelude::*},
     path::PathBuf,
 };
 
 use scopeguard::defer;
 use windows::core::{Interface, GUID, PCWSTR, PWSTR};
 
-use crate::{into_unknown, Error, TrashContext, TrashItem};
+use crate::{Error, TrashContext, TrashItem};
 
-use windows::Win32::{
-    Foundation::*, System::Com::*, System::Ole::*, System::Time::*, UI::Shell::Common::*,
-    UI::Shell::PropertiesSystem::*, UI::Shell::*,
-};
+use windows::Win32::{Foundation::*, System::Com::*, UI::Shell::PropertiesSystem::*, UI::Shell::*};
 
 ///////////////////////////////////////////////////////////////////////////
 // These don't have bindings in windows-rs for some reason
@@ -30,9 +23,6 @@ const SCID_ORIGINAL_LOCATION: PROPERTYKEY =
     PROPERTYKEY { fmtid: PSGUID_DISPLACED, pid: PID_DISPLACED_FROM };
 const SCID_DATE_DELETED: PROPERTYKEY =
     PROPERTYKEY { fmtid: PSGUID_DISPLACED, pid: PID_DISPLACED_DATE };
-// {43826D1E-E718-42EE-BC55-A1E261C37BFE}
-const IID_IShellItem: GUID =
-    GUID::from_values(0x43826d1e, 0xe718, 0x42ee, [0xbc, 0x55, 0xa1, 0xe2, 0x61, 0xc3, 0x7b, 0xfe]);
 
 const FOF_SILENT: u32 = 0x0004;
 const FOF_NOCONFIRMATION: u32 = 0x0010;
@@ -69,7 +59,7 @@ impl TrashContext {
 
             for full_path in full_paths.iter() {
                 let path_prefix = ['\\' as u16, '\\' as u16, '?' as u16, '\\' as u16];
-                let mut wide_path_container: Vec<_> =
+                let wide_path_container: Vec<_> =
                     full_path.as_os_str().encode_wide().chain(std::iter::once(0)).collect();
                 let wide_path_slice = if wide_path_container.starts_with(&path_prefix) {
                     &wide_path_container[path_prefix.len()..]
@@ -91,35 +81,16 @@ impl TrashContext {
 pub fn list() -> Result<Vec<TrashItem>, Error> {
     ensure_com_initialized();
     unsafe {
-        // let recycle_bin: IShellFolder2 = bind_to_csidl(CSIDL_BITBUCKET as c_int)?;
-        // let mut peidl = MaybeUninit::<Option<IEnumIDList>>::uninit();
-        // let flags = SHCONTF_FOLDERS.0 | SHCONTF_NONFOLDERS.0;
-
-        // let hr = recycle_bin.EnumObjects(HWND::default(), flags as u32, peidl.as_mut_ptr());
-        // // WARNING `hr.is_ok()` is DIFFERENT from `hr == S_OK`, because
-        // // `is_ok` returns true if the HRESULT as any of the several success codes
-        // // but here we want to be more strict and only accept S_OK.
-        // if hr != S_OK {
-        //     return Err(Error::Unknown {
-        //         description: format!(
-        //             "`EnumObjects` returned with HRESULT {:X}, but 0x0 was expected.",
-        //             hr.0
-        //         ),
-        //     });
-        // }
-        // let peidl = peidl.assume_init().ok_or_else(|| Error::Unknown {
-        //     description: "`EnumObjects` set its output to None.".into(),
-        // })?;
         let mut item_vec = Vec::new();
 
-        // TODO: make sure we free this
+        // TODO: make sure we free this?
         let mut recycle_bin = MaybeUninit::<Option<IShellItem2>>::uninit();
 
         SHGetKnownFolderItem(
             &FOLDERID_RecycleBinFolder,
             KF_FLAG_DEFAULT,
             HANDLE::default(),
-            &IID_IShellItem,
+            &IShellItem::IID,
             recycle_bin.as_mut_ptr() as *mut *mut c_void,
         )?;
 
@@ -139,21 +110,14 @@ pub fn list() -> Result<Vec<TrashItem>, Error> {
             // arr.len()
             match &arr[0] {
                 Some(item) => {
-                    // TODO: is SIGDN_DESKTOPABSOLUTEPARSING equivalent to SHGDN_FORPARSING?
-                    let id = get_display_name_new(item, SIGDN_DESKTOPABSOLUTEPARSING)?;
-                    dbg!(&id);
-                    let name = get_display_name_new(item, SIGDN_PARENTRELATIVE)?;
-                    dbg!(&name);
+                    let id = get_display_name(item, SIGDN_DESKTOPABSOLUTEPARSING)?;
+                    let name = get_display_name(item, SIGDN_PARENTRELATIVE)?;
                     let item2: IShellItem2 = item.cast()?;
-                    // TODO: free this?
+                    // TODO: free these?
                     let original_location_variant = item2.GetProperty(&SCID_ORIGINAL_LOCATION)?;
                     let original_location_bstr = PropVariantToBSTR(&original_location_variant)?;
                     let original_location = OsString::from_wide(original_location_bstr.as_wide());
-
-                    // let date_deleted_variant = item2.GetProperty(&SCID_DATE_DELETED)?;
-                    // let date_deleted = PropVariantToFileTime(&date_deleted_variant, PSTIME_FLAGS)?;
-                    let date_deleted = get_date_unix_new(&item2)?;
-                    // dbg!(date_deleted);
+                    let date_deleted = get_date_deleted_unix(&item2)?;
 
                     item_vec.push(TrashItem {
                         id,
@@ -161,10 +125,8 @@ pub fn list() -> Result<Vec<TrashItem>, Error> {
                             .into_string()
                             .map_err(|original| Error::ConvertOsString { original })?,
                         original_parent: PathBuf::from(original_location),
-                        time_deleted: date_deleted, // TODO get actual value
+                        time_deleted: date_deleted,
                     });
-                    // PropVariantChangeType()
-                    // dbg!(original_location);
                 }
                 None => {
                     break;
@@ -172,28 +134,6 @@ pub fn list() -> Result<Vec<TrashItem>, Error> {
             }
         }
 
-        // while let Ok(_) = peidl.Next(&mut [item_uninit.as_mut_ptr()], &mut count) {
-        //     if count > 0 {
-        //         let mut item = item_uninit.assume_init();
-
-        //         let id = get_display_name((&recycle_bin).into(), &mut item, SHGDN_FORPARSING)?;
-        //         let name = get_display_name((&recycle_bin).into(), &mut item, SHGDN_INFOLDER)?;
-
-        //         let orig_loc =
-        //             get_detail(&recycle_bin, &mut item, &SCID_ORIGINAL_LOCATION as *const _)?;
-        //         let date_deleted =
-        //             get_date_unix(&recycle_bin, &mut item, &SCID_DATE_DELETED as *const _)?;
-
-        //         item_vec.push(TrashItem {
-        //             id,
-        //             name: name
-        //                 .into_string()
-        //                 .map_err(|original| Error::ConvertOsString { original })?,
-        //             original_parent: PathBuf::from(orig_loc),
-        //             time_deleted: date_deleted,
-        //         });
-        //     }
-        // }
         Ok(item_vec)
     }
 }
@@ -204,37 +144,15 @@ where
 {
     ensure_com_initialized();
     unsafe {
-        let recycle_bin: IShellFolder2 = bind_to_csidl(CSIDL_BITBUCKET as i32)?;
         let pfo: IFileOperation = CoCreateInstance(&FileOperation as *const _, None, CLSCTX_ALL)?;
         pfo.SetOperationFlags(FOF_NO_UI)?;
         let mut at_least_one = false;
         for item in items {
             at_least_one = true;
-            let mut id_wstr: Vec<_> = item.id.encode_wide().chain(std::iter::once(0)).collect();
-            let mut pidl = MaybeUninit::<*mut ITEMIDLIST>::uninit();
-            recycle_bin.ParseDisplayName(
-                HWND::default(),
-                None,
-                PCWSTR(id_wstr.as_mut_ptr()),
-                std::ptr::null_mut(),
-                pidl.as_mut_ptr(),
-                std::ptr::null_mut(),
-            )?;
-            let pidl = pidl.assume_init();
-            defer! {{ CoTaskMemFree(pidl as *mut c_void); }}
-
-            let trash_item = CoTaskMemAlloc(size_of::<IShellItem>()) as *mut IShellItem;
-            defer! {{ CoTaskMemFree(trash_item as *mut c_void); }}
-
-            SHCreateItemWithParent(
-                std::ptr::null(),
-                &recycle_bin,
-                pidl,
-                &IShellItem::IID,
-                &mut (trash_item as *mut c_void),
-            )?;
-
-            pfo.DeleteItem(&*trash_item, None)?;
+            let id_as_wide: Vec<u16> = item.id.encode_wide().chain(std::iter::once(0)).collect();
+            let parsing_name = PCWSTR(id_as_wide.as_ptr());
+            let trash_item: IShellItem = SHCreateItemFromParsingName(&parsing_name, None)?;
+            pfo.DeleteItem(&trash_item, None)?;
         }
         if at_least_one {
             pfo.PerformOperations()?;
@@ -264,34 +182,12 @@ where
     }
     ensure_com_initialized();
     unsafe {
-        let recycle_bin: IShellFolder2 = bind_to_csidl(CSIDL_BITBUCKET as i32)?;
         let pfo: IFileOperation = CoCreateInstance(&FileOperation as *const _, None, CLSCTX_ALL)?;
         pfo.SetOperationFlags(FOF_NO_UI | FOFX_EARLYFAILURE)?;
         for item in items.iter() {
-            let id_wstr: Vec<_> = item.id.encode_wide().chain(std::iter::once(0)).collect();
-            let mut pidl = MaybeUninit::<*mut ITEMIDLIST>::uninit();
-            recycle_bin.ParseDisplayName(
-                HWND::default(),
-                None,
-                PCWSTR(id_wstr.as_ptr()),
-                std::ptr::null_mut(),
-                pidl.as_mut_ptr(),
-                std::ptr::null_mut(),
-            )?;
-            let pidl = pidl.assume_init();
-            defer! {{ CoTaskMemFree(pidl as *mut c_void); }}
-
-            let trash_item = CoTaskMemAlloc(size_of::<IShellItem>()) as *mut IShellItem;
-            defer! {{ CoTaskMemFree(trash_item as *mut c_void); }}
-
-            SHCreateItemWithParent(
-                std::ptr::null(),
-                &recycle_bin,
-                pidl,
-                &IShellItem::IID,
-                &mut (trash_item as *mut c_void),
-            )?;
-
+            let id_as_wide: Vec<u16> = item.id.encode_wide().chain(std::iter::once(0)).collect();
+            let parsing_name = PCWSTR(id_as_wide.as_ptr());
+            let trash_item: IShellItem = SHCreateItemFromParsingName(&parsing_name, None)?;
             let parent_path_wide: Vec<_> =
                 item.original_parent.as_os_str().encode_wide().chain(std::iter::once(0)).collect();
             let orig_folder_shi: IShellItem =
@@ -301,7 +197,7 @@ where
                 .chain(std::iter::once(0))
                 .collect();
 
-            pfo.MoveItem(&*trash_item, orig_folder_shi, PCWSTR(name_wstr.as_ptr()), None)?;
+            pfo.MoveItem(trash_item, orig_folder_shi, PCWSTR(name_wstr.as_ptr()), None)?;
         }
         if !items.is_empty() {
             pfo.PerformOperations()?;
@@ -310,26 +206,11 @@ where
     }
 }
 
-unsafe fn get_display_name(
-    psf: IShellFolder,
-    pidl: *mut ITEMIDLIST,
-    flags: _SHGDNF,
-) -> Result<OsString, Error> {
-    let mut sr = psf.GetDisplayNameOf(pidl, flags.0 as u32)?;
-    let mut name = MaybeUninit::<PWSTR>::uninit();
-    StrRetToStrW(&mut sr as *mut _, pidl, name.as_mut_ptr())?;
-    let name = name.assume_init();
-    let result = wstr_to_os_string(name);
-    CoTaskMemFree(name.0 as *mut c_void);
-    Ok(result)
-}
-
-unsafe fn get_display_name_new(psi: &IShellItem, sigdnname: SIGDN) -> Result<OsString, Error> {
+unsafe fn get_display_name(psi: &IShellItem, sigdnname: SIGDN) -> Result<OsString, Error> {
     let name = psi.GetDisplayName(sigdnname)?;
-    let ret = wstr_to_os_string(name);
-    // todo: is this safe? not totally sure whether wstr_to_os_string is cloning the string
+    let result = wstr_to_os_string(name);
     CoTaskMemFree(name.0 as *const c_void);
-    Ok(ret)
+    Ok(result)
 }
 
 unsafe fn wstr_to_os_string(wstr: PWSTR) -> OsString {
@@ -341,119 +222,17 @@ unsafe fn wstr_to_os_string(wstr: PWSTR) -> OsString {
     OsString::from_wide(wstr_slice)
 }
 
-unsafe fn get_detail(
-    psf: &IShellFolder2,
-    pidl: *mut ITEMIDLIST,
-    pscid: *const PROPERTYKEY,
-) -> Result<OsString, Error> {
-    let vt = psf.GetDetailsEx(pidl, pscid)?;
-    let mut vt = scopeguard::guard(vt, |mut vt| {
-        // Ignoring the return value
-        let _ = VariantClear(&mut vt as *mut _);
-    });
-    VariantChangeType(vt.deref_mut() as *mut _, vt.deref_mut() as *mut _, 0, VT_BSTR.0 as u16)?;
-    let pstr = &vt.Anonymous.Anonymous.Anonymous.bstrVal;
-    Ok(OsString::from_wide(pstr.as_wide()))
-}
+unsafe fn get_date_deleted_unix(item: &IShellItem2) -> Result<i64, Error> {
+    /// January 1, 1970 as Windows file time
+    const EPOCH_AS_FILETIME: u64 = 116444736000000000;
+    const HUNDREDS_OF_NANOSECONDS: u64 = 10000000;
 
-unsafe fn get_date_unix_new(
-    item: &IShellItem2
-) -> Result<i64, Error> {
-    // let vt = psf.GetDetailsEx(pidl, pscid)?;
-    let vt = item.GetProperty(&SCID_DATE_DELETED)?;
-    
-    // let mut vt = scopeguard::guard(vt, |mut vt| {
-    //     // Ignoring the return value
-    //     let _ = VariantClear(&mut vt as *mut _);
-    // });
-    // VariantChangeType(vt.deref_mut() as *mut _, vt.deref_mut() as *mut _, 0, VT_DATE.0 as u16)?;
-    let date = vt.Anonymous.Anonymous.Anonymous.date;
-    let unix_time = variant_time_to_unix_time(date)?;
-    Ok(unix_time)
-}
+    let time = item.GetFileTime(&SCID_DATE_DELETED)?;
+    let time_u64 = ((time.dwHighDateTime as u64) << 32) | (time.dwLowDateTime as u64);
+    let rel_to_linux_epoch = time_u64 - EPOCH_AS_FILETIME;
+    let seconds_since_unix_epoch = rel_to_linux_epoch / HUNDREDS_OF_NANOSECONDS;
 
-unsafe fn get_date_unix(
-    psf: &IShellFolder2,
-    pidl: *mut ITEMIDLIST,
-    pscid: *const PROPERTYKEY,
-) -> Result<i64, Error> {
-    let vt = psf.GetDetailsEx(pidl, pscid)?;
-    let mut vt = scopeguard::guard(vt, |mut vt| {
-        // Ignoring the return value
-        let _ = VariantClear(&mut vt as *mut _);
-    });
-    VariantChangeType(vt.deref_mut() as *mut _, vt.deref_mut() as *mut _, 0, VT_DATE.0 as u16)?;
-    let date = vt.Anonymous.Anonymous.Anonymous.date;
-    let unix_time = variant_time_to_unix_time(date)?;
-    Ok(unix_time)
-}
-
-unsafe fn variant_time_to_unix_time(from: f64) -> Result<i64, Error> {
-    #[repr(C)]
-    #[derive(Clone, Copy)]
-    struct LargeIntegerParts {
-        low_part: u32,
-        high_part: u32,
-    }
-    #[repr(C)]
-    union LargeInteger {
-        parts: LargeIntegerParts,
-        whole: u64,
-    }
-    let mut st = MaybeUninit::<SYSTEMTIME>::uninit();
-    if 0 == VariantTimeToSystemTime(from, st.as_mut_ptr()) {
-        return Err(Error::Unknown {
-            description: format!(
-                "`VariantTimeToSystemTime` indicated failure for the parameter {:?}",
-                from
-            ),
-        });
-    }
-    let st = st.assume_init();
-    let mut ft = MaybeUninit::<FILETIME>::uninit();
-    if SystemTimeToFileTime(&st, ft.as_mut_ptr()) == false {
-        return Err(Error::Unknown {
-            description: format!("`SystemTimeToFileTime` failed with: {:?}", GetLastError()),
-        });
-    }
-    let ft = ft.assume_init();
-
-    let large_int = LargeInteger {
-        parts: LargeIntegerParts { low_part: ft.dwLowDateTime, high_part: ft.dwHighDateTime },
-    };
-
-    // Applying assume init straight away because there's no explicit support to initialize struct
-    // fields one-by-one in an `MaybeUninit` as of Rust 1.39.0
-    // See: https://github.com/rust-lang/rust/blob/1.39.0/src/libcore/mem/maybe_uninit.rs#L170
-    // let mut uli = MaybeUninit::<ULARGE_INTEGER>::zeroed().assume_init();
-    // {
-    //     let u_mut = uli.u_mut();
-    //     u_mut.LowPart = ft.dwLowDateTime;
-    //     u_mut.HighPart = std::mem::transmute(ft.dwHighDateTime);
-    // }
-    let windows_ticks: u64 = large_int.whole;
-    Ok(windows_ticks_to_unix_seconds(windows_ticks))
-}
-
-fn windows_ticks_to_unix_seconds(windows_ticks: u64) -> i64 {
-    // Fun fact: if my calculations are correct, then storing sucn ticks in an
-    // i64 can remain valid until about 6000 years from the very first tick
-    const WINDOWS_TICK: u64 = 10000000;
-    const SEC_TO_UNIX_EPOCH: i64 = 11644473600;
-    (windows_ticks / WINDOWS_TICK) as i64 - SEC_TO_UNIX_EPOCH
-}
-
-unsafe fn bind_to_csidl<T: Interface>(csidl: c_int) -> Result<T, Error> {
-    let pidl = SHGetSpecialFolderLocation(HWND::default(), csidl)?;
-    defer! {{ CoTaskMemFree(pidl as _); }};
-
-    let desktop = SHGetDesktopFolder()?;
-    if (*pidl).mkid.cb != 0 {
-        let target: T = desktop.BindToObject(pidl, None)?;
-        Ok(target)
-    } else {
-        Ok(desktop.cast().map_err(into_unknown)?)
-    }
+    Ok(seconds_since_unix_epoch as i64)
 }
 
 struct CoInitializer {}
