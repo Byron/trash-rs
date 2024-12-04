9ed83e724f944f4eacf2e4cafdf8025548f7a17b
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-04 16:51:29 +0700

    doc: replace create with create_new to avoid potentially nulling existing files

--- examples/delete_method.rs
+++ examples/delete_method.rs
@@ -17,7 +17,7 @@ fn main() {
     trash_ctx.set_delete_method(DeleteMethod::NsFileManager);
 
     let path = "this_file_was_deleted_using_the_ns_file_manager";
-    File::create(path).unwrap();
+    File::create_new(path).unwrap();
     trash_ctx.delete(path).unwrap();
     assert!(File::open(path).is_err());
 }
