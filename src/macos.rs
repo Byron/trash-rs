8c481cd643ec413657b2b45fa4c5cebf22c84c6d
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-06 19:54:33 +0700

    add esc_quote

--- src/macos.rs
+++ src/macos.rs
@@ -180,6 +180,22 @@ fn percent_encode(input: &[u8]) -> Cow<'_, str> {
     Cow::Owned(res)
 }
 
+/// Escapes `"` or `\` with `\` for use in AppleScript text
+fn esc_quote(s: &str) -> Cow<'_, str> {
+  if s.contains(&['"','\\']) {
+    let mut r = String::with_capacity(s.len());
+    let chars = s.chars();
+    for c in chars { match c {
+       '"'
+      |'\\' => {r.push('\\');r.push(c);}, // escapes quote/escape char
+      _     => {             r.push(c);}, // no escape required
+    }};
+    Cow::Owned   (r)
+  } else {
+    Cow::Borrowed(s)
+  }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{
