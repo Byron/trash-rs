83c262383c4930e15502a5f89eda690d1ea3c45c
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-07 20:55:21 +0700

    add comparison to cli osascript
    and return selected items

--- src/macos/test_macos.rs
+++ src/macos/test_macos.rs
@@ -1,6 +1,54 @@
+use std::process::Command;
+use std::ffi::OsString;
+use crate::into_unknown;
 use crate::tests::init_logging;
 use crate::Error;
 use serial_test::serial;
+
+fn test_run_as_with_finder_cli() -> Result<(), Error> {
+    let mut command = Command::new("osascript");
+
+    let script_text = format!(
+        r#"
+        tell application "Finder"
+            set Trash_items to selection
+        end tell
+        if (class of Trash_items) is not list then -- if only 1 file is deleted, returns a file, not a list
+            return                   (POSIX path of (Trash_items as alias))
+        end if
+        repeat with aFile in Trash_items -- Finder reference
+            set contents of aFile to (POSIX path of (aFile as alias)) -- can't get paths of Finder reference, coersion to alias needed
+        end repeat
+        return Trash_items
+                              "#
+    );
+
+    let argv: Vec<OsString> = vec!["-e".into(), script_text.into()];
+    command.args(argv);
+
+    // Execute command
+    let result = command.output().map_err(into_unknown)?;
+    let stdout = String::from_utf8_lossy(&result.stdout);
+    println!("stdout={stdout:?}");
+    if !result.status.success() {
+        let stderr = String::from_utf8_lossy(&result.stderr);
+        match result.status.code() {
+            None => {
+                return Err(Error::Unknown {
+                    description: format!("The AppleScript exited with error. stderr: {}", stderr),
+                })
+            }
+
+            Some(code) => {
+                return Err(Error::Os {
+                    code,
+                    description: format!("The AppleScript exited with error. stderr: {}", stderr),
+                })
+            }
+        };
+    }
+    Ok(())
+}
 
 fn test_run_as_with_finder() -> Result<(), Error> {
     use osakit::{Language, Script};
@@ -9,17 +57,23 @@ fn test_run_as_with_finder() -> Result<(), Error> {
         tell application "Finder"
             set Trash_items to selection
         end tell
-        return "empty string"
+        if (class of Trash_items) is not list then -- if only 1 file is deleted, returns a file, not a list
+            return                   (POSIX path of (Trash_items as alias))
+        end if
+        repeat with aFile in Trash_items -- Finder reference
+            set contents of aFile to (POSIX path of (aFile as alias)) -- can't get paths of Finder reference, coersion to alias needed
+        end repeat
+        return Trash_items
                               "#
     );
 
     let mut script = Script::new_from_source(Language::AppleScript, &script_text);
 
     // Compile and Execute script
-    println!("  ? script starting...");
+    // println!("  ? script starting...");
     match script.compile() {
         Ok(_) => {
-            println!("  ✓ script compiled");
+            // println!("  ✓ script compiled");
             match script.execute() {
                 Ok(res) => println!("  ✓ script run res={:?}", &res),
                 Err(e) => {
@@ -41,10 +95,11 @@ use std::{thread, time};
 #[serial]
 fn test_x1() {
     init_logging();
-    let ten_millis = time::Duration::from_millis(1000);
-    for i in 0..10 {
-        println!("{i} sleeping");
-        thread::sleep(ten_millis);
+    let ten_millis = time::Duration::from_millis(100);
+    for i in 0..10 { println!("{i} sleeping cli"); // thread::sleep(ten_millis);
+        test_run_as_with_finder_cli().unwrap();
+    }
+    for i in 0..10 { println!("{i} sleeping"); thread::sleep(ten_millis);
         test_run_as_with_finder().unwrap();
     }
 }
