2343235a3211f868633884873e080c5d1fe7c51a
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-07 19:25:13 +0700

    bugs: simplify test

--- src/macos/test_macos.rs
+++ src/macos/test_macos.rs
@@ -6,101 +6,19 @@ use crate::{
 use serial_test::serial;
 use std::path::PathBuf;
 
-
+use std::{thread, time};
 #[test] #[serial] fn test_x1 () {
-    use std::{thread, time};
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(3000);
-    thread::sleep(ten_millis);
     init_logging();
-    let mut trash_ctx = TrashContext::default();
-    trash_ctx.set_delete_method(DeleteMethod::Finder);
+    for i in 0..10 {
+        println!("{i} sleeping");
+        let ten_millis = time::Duration::from_millis(1000);
+        thread::sleep(ten_millis);
 
-    let path1 = PathBuf::from(get_unique_name());
-    let path2 = PathBuf::from(get_unique_name());
-    trash_ctx.delete_all(&[&path1, &path2]).unwrap();
-}
+        let mut trash_ctx = TrashContext::default();
+        trash_ctx.set_delete_method(DeleteMethod::Finder);
 
-#[test] #[serial] fn test_x2 () {
-    use std::{thread, time};
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(3000);
-    thread::sleep(ten_millis);
-    init_logging();
-    let mut trash_ctx = TrashContext::default();
-    trash_ctx.set_delete_method(DeleteMethod::Finder);
-
-    let path1 = PathBuf::from(get_unique_name());
-    let path2 = PathBuf::from(get_unique_name());
-    trash_ctx.delete_all(&[&path1, &path2]).unwrap();
-}
-
-#[test] #[serial] fn test_x3 () {
-    use std::{thread, time};
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(3000);
-    thread::sleep(ten_millis);
-    init_logging();
-    let mut trash_ctx = TrashContext::default();
-    trash_ctx.set_delete_method(DeleteMethod::Finder);
-
-    let path1 = PathBuf::from(get_unique_name());
-    let path2 = PathBuf::from(get_unique_name());
-    trash_ctx.delete_all(&[&path1, &path2]).unwrap();
-}
-
-#[test] #[serial] fn test_x4 () {
-    use std::{thread, time};
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(3000);
-    thread::sleep(ten_millis);
-    init_logging();
-    let mut trash_ctx = TrashContext::default();
-    trash_ctx.set_delete_method(DeleteMethod::Finder);
-
-    let path1 = PathBuf::from(get_unique_name());
-    let path2 = PathBuf::from(get_unique_name());
-    trash_ctx.delete_all(&[&path1, &path2]).unwrap();
-}
-
-#[test] #[serial] fn test_x5 () {
-    use std::{thread, time};
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(3000);
-    thread::sleep(ten_millis);
-    init_logging();
-    let mut trash_ctx = TrashContext::default();
-    trash_ctx.set_delete_method(DeleteMethod::Finder);
-
-    let path1 = PathBuf::from(get_unique_name());
-    let path2 = PathBuf::from(get_unique_name());
-    trash_ctx.delete_all(&[&path1, &path2]).unwrap();
-}
-
-#[test] #[serial] fn test_x6 () {
-    use std::{thread, time};
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(3000);
-    thread::sleep(ten_millis);
-    init_logging();
-    let mut trash_ctx = TrashContext::default();
-    trash_ctx.set_delete_method(DeleteMethod::Finder);
-
-    let path1 = PathBuf::from(get_unique_name());
-    let path2 = PathBuf::from(get_unique_name());
-    trash_ctx.delete_all(&[&path1, &path2]).unwrap();
-}
-
-#[test] #[serial] fn test_x7 () {
-    use std::{thread, time};
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(3000);
-    thread::sleep(ten_millis);
-    init_logging();
-    let mut trash_ctx = TrashContext::default();
-    trash_ctx.set_delete_method(DeleteMethod::Finder);
-
-    let path1 = PathBuf::from(get_unique_name());
-    let path2 = PathBuf::from(get_unique_name());
-    trash_ctx.delete_all(&[&path1, &path2]).unwrap();
+        let path1 = PathBuf::from(get_unique_name());
+        let path2 = PathBuf::from(get_unique_name());
+        trash_ctx.delete_all(&[&path1, &path2]).unwrap();
+    }
 }
