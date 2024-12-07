c54bf01af9f02d17337be877efb55a8d09988a70
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-07 20:33:31 +0700

    remove nonmac tests

--- tests/trash.rs
+++ tests/trash.rs
@@ -1,5 +1,3 @@
-use trash::delete;
-
 mod util {
     use std::sync::atomic::{AtomicI32, Ordering};
 
@@ -19,56 +17,3 @@ mod util {
     }
 }
 pub use util::{get_unique_name, init_logging};
-
-#[cfg(unix)]
-mod unix {
-    use log::trace;
-    use std::{
-        fs::{create_dir, remove_dir_all, remove_file, File},
-        os::unix::fs::symlink,
-        path::Path,
-    };
-
-    use super::{get_unique_name, init_logging};
-    use crate::delete;
-    // use crate::init_logging;
-
-    #[test]
-    fn test_delete_symlink() {
-        init_logging();
-        trace!("Started test_delete_symlink");
-        let target_path = get_unique_name();
-        File::create_new(&target_path).unwrap();
-
-        let link_path = "test_link_to_delete";
-        symlink(&target_path, link_path).unwrap();
-
-        delete(link_path).unwrap();
-        assert!(File::open(link_path).is_err());
-        assert!(File::open(&target_path).is_ok());
-        // Cleanup
-        remove_file(&target_path).unwrap();
-        trace!("Finished test_delete_symlink");
-    }
-
-    #[test]
-    fn test_delete_symlink_in_folder() {
-        init_logging();
-        trace!("Started test_delete_symlink_in_folder");
-        let target_path = "test_link_target_for_delete_from_folder";
-        File::create_new(target_path).unwrap();
-
-        let folder = Path::new("test_parent_folder_for_link_to_delete");
-        create_dir(folder).unwrap();
-        let link_path = folder.join("test_link_to_delete_from_folder");
-        symlink(target_path, &link_path).unwrap();
-
-        delete(&link_path).unwrap();
-        assert!(File::open(link_path).is_err());
-        assert!(File::open(target_path).is_ok());
-        // Cleanup
-        remove_file(target_path).unwrap();
-        remove_dir_all(folder).unwrap();
-        trace!("Finished test_delete_symlink_in_folder");
-    }
-}
