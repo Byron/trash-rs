095f92dece5fda932492d1fa3132ce6a1318cb99
eugenesvk <eugenesvk@users.noreply.github.com>
2024-12-07 20:14:58 +0700

    _remove all non-mac tests

--- tests/trash.rs
+++ tests/trash.rs
@@ -1,10 +1,4 @@
-use std::fs::{create_dir, File};
-use std::path::{Path, PathBuf};
-
-use log::trace;
-
-use serial_test::serial;
-use trash::{delete, delete_all};
+use trash::delete;
 
 mod util {
     use std::sync::atomic::{AtomicI32, Ordering};
@@ -25,60 +19,6 @@ mod util {
     }
 }
 pub use util::{get_unique_name, init_logging};
-
-#[test]
-#[serial]
-fn test_delete_file() {
-    init_logging();
-    println!("Started test_delete_file");
-
-    let path = PathBuf::from(get_unique_name());
-    File::create_new(&path).unwrap();
-    println!("created file {:?}", &path);
-    assert!(path.exists());
-
-    println!("trying to delete {:?}", &path);
-    delete(&path).unwrap();
-    assert!(!path.exists());
-    println!("Finished test_delete_file");
-}
-
-#[test]
-#[serial]
-fn test_delete_folder() {
-    init_logging();
-    trace!("Started test_delete_folder");
-
-    let path = PathBuf::from(get_unique_name());
-    create_dir(&path).unwrap();
-    File::create_new(path.join("file_in_folder")).unwrap();
-
-    assert!(path.exists());
-    delete(&path).unwrap();
-    assert!(!path.exists());
-
-    trace!("Finished test_delete_folder");
-}
-
-#[test]
-fn test_delete_all() {
-    init_logging();
-    println!("Started test_delete_all");
-    let count: usize = 3;
-
-    let paths: Vec<_> = (0..count).map(|i| format!("test_file_to_delete_{i}")).collect();
-    for path in paths.iter() {
-        File::create_new(path).unwrap();
-    }
-    println!("created test_delete_all");
-
-    delete_all(&paths).unwrap();
-    println!("deleted test_delete_all");
-    for path in paths.iter() {
-        assert!(File::open(path).is_err());
-    }
-    trace!("Finished test_delete_all");
-}
 
 #[cfg(unix)]
 mod unix {
@@ -131,52 +71,4 @@ mod unix {
         remove_dir_all(folder).unwrap();
         trace!("Finished test_delete_symlink_in_folder");
     }
-}
-
-#[test]
-#[serial]
-fn create_remove_single_file() {
-    // Let's create and remove a single file
-    let name = get_unique_name();
-    File::create_new(&name).unwrap();
-    trash::delete(&name).unwrap();
-    assert!(File::open(&name).is_err());
-}
-
-#[cfg(all(unix, not(target_os = "macos"), not(target_os = "ios"), not(target_os = "android")))]
-#[test]
-#[serial]
-fn create_remove_single_file_invalid_utf8() {
-    use std::ffi::OsStr;
-    let name = unsafe { OsStr::from_encoded_bytes_unchecked(&[168]) };
-    File::create_new(name).unwrap();
-    trash::delete(name).unwrap();
-}
-
-#[test]
-fn recursive_file_deletion() {
-    let parent_dir = Path::new("remove-me");
-    let dir1 = parent_dir.join("dir1");
-    let dir2 = parent_dir.join("dir2");
-    std::fs::create_dir_all(&dir1).unwrap();
-    std::fs::create_dir_all(&dir2).unwrap();
-    File::create_new(dir1.join("same-name")).unwrap();
-    File::create_new(dir2.join("same-name")).unwrap();
-
-    trash::delete(parent_dir).unwrap();
-    assert!(!parent_dir.exists());
-}
-
-#[test]
-fn recursive_file_with_content_deletion() {
-    let parent_dir = Path::new("remove-me-content");
-    let dir1 = parent_dir.join("dir1");
-    let dir2 = parent_dir.join("dir2");
-    std::fs::create_dir_all(&dir1).unwrap();
-    std::fs::create_dir_all(&dir2).unwrap();
-    File::create_new(dir1.join("same-name")).unwrap();
-    std::fs::write(dir2.join("same-name"), b"some content").unwrap();
-
-    trash::delete(parent_dir).unwrap();
-    assert!(!parent_dir.exists());
 }
